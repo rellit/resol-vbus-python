9e458be150d7d7980057a04d14d06401240af1d8
hmueller01 <github@euhm.de>
2020-10-16 21:26:58 +0200

    add serial line (UART) mode

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
+import os
+
+# configure kind of connection "lan" or "serial"
+connection = "lan"
+
+# only used for "lan"
 address = ("192.168.1.253", 7053)
 vbus_pass = "vbus"
-spec_file = 'spec/DeltaSolBS2009.json'
-debug = False
+
+# only used for "serial"
+port = "/dev/ttyAMA0"
+baudrate = 9600
+
+spec_file = os.path.dirname(__file__) + '/spec/DeltaSolBS2009.json'
+# expected amount of different source packets (see spec_file)
 expected_packets = 1
+
+debug = False
